e7ce4f7ee4802f82820e354e8815e206a64b6c8a
Jaayyb <43382002+Jaayyb@users.noreply.github.com>
2018-09-20 17:15:48 +0100

    Update Lab2Ex4.java
    Finished ex4 at home

--- src/Lab2Ex4.java
+++ src/Lab2Ex4.java
@@ -10,10 +10,13 @@ public class Lab2Ex4 {
     {
         String name = JOptionPane.showInputDialog("Please enter your full name: ");
         char initial = name.charAt(0);
+        int surnameNo = 0;
         String nameCaps = name.toUpperCase();
+        String surname;
+        surnameNo = name.lastIndexOf(' ');
 
         JOptionPane.showMessageDialog(null, "The number of characters in the name is: " + name.length() + "\nThe first initial of the name is " + initial
-        + "\nThe name in uppercase is: " +nameCaps);
+                + "\nThe name in uppercase is: " +nameCaps + "\nThe surname by itself is: " + name.substring(surnameNo));
 
     }
 }
