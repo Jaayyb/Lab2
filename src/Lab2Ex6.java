03c573567fb6fdd479edf5ed3c73423c3749d167
Jaayyb <43382002+Jaayyb@users.noreply.github.com>
2018-09-20 19:56:59 +0100

    Update Lab2Ex6.java
    Updated the code as I originally solved the problem doing everything in a user defined method rather than the main.

--- src/Lab2Ex6.java
+++ src/Lab2Ex6.java
@@ -3,22 +3,22 @@ import java.awt.*;
 public class Lab2Ex6 {
     public static void main(String[] args) {
 
-        int number = 1;
-        getCube(number);
-    }
-
-    public static void getCube(int x) {
-
         String txtAreaString;
         JTextArea txtArea = new JTextArea();
         txtArea.append("Number \tCube\n-----------\t-----------");
 
-        for (x = 1; x < 16; x++) {
+        for (int x = 1; x < 16; x++) {
 
-            int cube = x * x * x;
-            txtArea.append("\n" + x + "\t" + cube);
+            txtArea.append("\n" + x + "\t" + getCube(x));
         }
 
         JOptionPane.showMessageDialog(null, txtArea);
+    }
+
+    public static int getCube(int x) {
+
+        x = x * x * x;
+
+        return x;
     }
 }
